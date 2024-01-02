25e929dc815a15c27616e489bbece1d8e4e31598
Starfosse <alexis.bouleau92@yahoo.fr>
2024-01-02 12:40:50 +0100

    update

--- clone-discord/src/server/index.ts
+++ clone-discord/src/server/index.ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/db"
 import { router, publicProcedure } from "./trpc"
 import { currentUser } from "@clerk/nextjs"
+import { ProfileValidator } from "@/lib/validator/profile-validator"
 
 export const appRouter = router({
   getTodos: publicProcedure.query(async () => {
@@ -14,6 +15,31 @@ export const appRouter = router({
       },
     })
   }),
+  updateUser: publicProcedure
+    .input(ProfileValidator)
+    .mutation(async ({ input }) => {
+      const user = await currentUser()
+      const { imageUrl, pseudo, state } = input
+      console.log("state ===")
+      console.log(state)
+      console.log("state ===")
+      const userToUpdate = await prisma.user.findFirst({
+        where: {
+          userId: user?.id,
+        },
+      })
+      return await prisma.user.update({
+        where: {
+          userId: user?.id,
+        },
+        data: {
+          imageUrl: imageUrl,
+          pseudo: pseudo,
+          state: state,
+        },
+      })
+      return
+    }),
 })
 
 export type AppRouter = typeof appRouter
