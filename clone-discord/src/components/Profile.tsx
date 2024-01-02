2e61be83fa529162e4bc22ec22d0030bad803276
Starfosse <alexis.bouleau92@yahoo.fr>
2024-01-02 15:37:36 +0100

    update

--- clone-discord/src/components/Profile.tsx
+++ clone-discord/src/components/Profile.tsx
@@ -1,10 +1,6 @@
 "use client"
 
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "./ui/avatar"
+import { trpc } from "@/app/_trpc/client"
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -16,40 +12,51 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { trpc } from "@/app/_trpc/client"
-import { Separator } from "./ui/separator"
-import {
-  useForm,
-  SubmitHandler,
-  Controller,
-} from "react-hook-form"
 import {
   ProfileValidator,
   TProfileValidator,
 } from "@/lib/validator/profile-validator"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { cn } from "@/lib/utils"
+import { stateList } from "@prisma/client"
+import { useForm } from "react-hook-form"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "./ui/avatar"
+import { Separator } from "./ui/separator"
+import {
+  ItemIndicator,
+  SelectItemIndicator,
+  SelectItemText,
+} from "@radix-ui/react-select"
 
 const Profile = () => {
   const { data, isLoading } = trpc.getUser.useQuery()
 
-  const {
-    register,
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm<TProfileValidator>({
+  const form = useForm<TProfileValidator>({
     resolver: zodResolver(ProfileValidator),
+    defaultValues: {
+      imageUrl: data?.imageUrl,
+      pseudo: data?.pseudo,
+      state: stateList.BUSY,
+    },
   })
 
   const { mutate } = trpc.updateUser.useMutation()
@@ -59,9 +66,6 @@ const Profile = () => {
     pseudo,
     state,
   }: TProfileValidator) => {
-    console.log("ok")
-    console.log(state)
-    console.log("ok")
     mutate({ imageUrl, pseudo, state })
   }
 
@@ -83,111 +87,128 @@ const Profile = () => {
           )}
         </DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <DialogHeader>
-              <DialogTitle>Modifier le profil</DialogTitle>
-              <DialogDescription>
-                Changez votre profil ici.
-              </DialogDescription>
-            </DialogHeader>
-            <div className="grid gap-4 py-4">
-              {/* <form onSubmit={handleSubmit(onSubmit)}> */}
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label
-                  htmlFor="imageUrl"
-                  className="text-right">
-                  Image de profil
-                </Label>
-                <Input
-                  {...register("imageUrl")}
-                  className={cn("col-span-3", {
-                    "focus-visible:ring-red-500":
-                      errors.imageUrl,
-                  })}
-                />
-                {/* {errors?.imageUrl && (
-                <p className="text-sm text-red-500">
-                  {errors.imageUrl.message}
-                </p>
-              )} */}
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label
-                  htmlFor="pseudo"
-                  className="text-right">
-                  Pseudo
-                </Label>
-                <Input
-                  {...register("pseudo")}
-                  className={cn("col-span-3", {
-                    "focus-visible:ring-red-500":
-                      errors.pseudo,
-                  })}
-                />
-                {/* {errors?.pseudo && (
-                <p className="text-sm text-red-500">
-                  {errors.pseudo.message}
-                </p>
-              )} */}
-              </div>
-              <div className="grid grid-cols-4 gap-4 items-center">
-                <Label
-                  htmlFor="state"
-                  className="text-right">
-                  Etat
-                </Label>
-                <Controller
-                  name="state"
-                  control={control}
-                  rules={{
-                    required: "ce champs est requis",
-                  }}
+          <DialogHeader>
+            <DialogTitle>Modifier le profil</DialogTitle>
+            <DialogDescription>
+              Ne modifiez que les champs voulus.
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="grid py-4">
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
                   render={({ field }) => (
-                    <Select {...field}>
-                      <SelectTrigger className="col-span-3">
-                        <SelectValue placeholder="En ligne (par défaut)" />{" "}
-                        {/* {`${data.etat}`}*/}
-                      </SelectTrigger>
-                      <SelectContent
-                        position="popper"
-                        side="right">
-                        <SelectGroup>
-                          <SelectItem value="ONLINE">
-                            En ligne
-                          </SelectItem>
-                          <Separator className="w-5/6 my-1 mx-auto" />
-                          <SelectItem value="ABSENT">
-                            Absent
-                          </SelectItem>
-                          <SelectItem value="BUSY">
-                            Occupé
-                            <p className="text-[0.6rem] text-muted-foreground">
-                              Tu ne recevras aucune
-                              notification
-                            </p>
-                          </SelectItem>
-                          <SelectItem value="OFFLINE">
-                            Deconnecté
-                            <p className="text-[0.6rem] text-muted-foreground">
-                              Tu n'apparaîtras pas connecté
-                            </p>
-                          </SelectItem>
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
+                    <FormItem className="grid grid-cols-4 items-center gap-x-4">
+                      <FormLabel className="text-right">
+                        Image de profil
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder={`${data?.imageUrl}`}
+                          {...field}
+                          className="col-span-3"
+                        />
+                      </FormControl>
+                      <FormMessage className="col-span-4 text-right" />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="pseudo"
+                  render={({ field }) => (
+                    <FormItem className="grid grid-cols-4 items-center gap-x-4">
+                      <FormLabel className="text-right">
+                        Pseudo
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder={`${data?.pseudo}`}
+                          {...field}
+                          className="col-span-3"
+                        />
+                      </FormControl>
+                      <FormMessage className="col-span-4 text-right" />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="state"
+                  render={({ field }) => (
+                    <FormItem className="grid grid-cols-4 items-center gap-4">
+                      <FormLabel className="text-right">
+                        Etat
+                      </FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}>
+                        <FormControl>
+                          <SelectTrigger className="col-span-3">
+                            <SelectValue placeholder="En ligne (par défaut)" />{" "}
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent
+                          position="popper"
+                          side="right"
+                          sideOffset={25}>
+                          <SelectGroup>
+                            <SelectItem
+                              value={stateList.ONLINE}
+                              key={stateList.ONLINE}
+                              slot="test">
+                              En ligne
+                            </SelectItem>
+
+                            <Separator className="w-5/6 my-1 mx-auto" />
+                            <SelectItem
+                              value={stateList.ABSENT}
+                              key={stateList.ABSENT}>
+                              Absent
+                            </SelectItem>
+                            <SelectItem
+                              value={stateList.BUSY}
+                              key={stateList.BUSY}
+                              className="text-left">
+                              Occupé
+                              {/* <p className="text-[0.6rem] text-muted-foreground text-right">
+                                Tu ne recevras aucune
+                                notification
+                              </p> */}
+                            </SelectItem>
+                            <SelectItem
+                              value={stateList.OFFLINE}
+                              key={stateList.OFFLINE}
+                              textValue="Tu n'apparaîtras pas
+                              connecté">
+                              Deconnecté
+                              {/* <p className="text-[0.6rem] text-muted-foreground">
+                                Tu n'apparaîtras pas
+                                connecté
+                              </p> */}
+                            </SelectItem>
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
                   )}
                 />
               </div>
-            </div>
-            <DialogFooter>
-              <Button>Sauvegarder</Button>
-            </DialogFooter>
-          </form>
+              <DialogFooter>
+                <Button>Sauvegarder</Button>
+              </DialogFooter>
+            </form>
+          </Form>
         </DialogContent>
       </Dialog>
     </>
   )
 }
 // Ajouter un carroussel d'image prédéfinies pour la photo de profil
+//ajouter preview image profile
 // Ajouter un sonner pour valider les modifs
+//ajouter une sous-description des state
 export default Profile
