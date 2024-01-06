c95896cafe556cb3d5dc7f5288da4c1a489493e3
Starfosse <alexis.bouleau92@yahoo.fr>
2024-01-06 11:27:31 +0100

    updatebtrpc

---
+++ clone-discord/src/components/ServerSideBar.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { useParams } from "next/navigation"
+import ServerLeftHeader from "./ServerLeftHeader"
+import { trpc } from "@/app/_trpc/client"
+import { Separator } from "./ui/separator"
+import ServerListChannel from "./ServerListChannel"
+
+const ServerSideBar = () => {
+  const serverId = useParams<{ serverId: string }>()
+  const { data: currentServer } =
+    trpc.getServer.useQuery(serverId)
+  // console.log(currentServer)
+  return (
+    <div className="flex h-full">
+      <div className="w-52 bg-secondaryColor sticky z-50 h-full flex flex-col px-2 py-2">
+        {currentServer && (
+          <ServerLeftHeader {...currentServer} />
+        )}
+        <Separator className=" w-4/5 my-4 justify-center mx-auto" />
+        {currentServer && (
+          <ServerListChannel {...currentServer} />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default ServerSideBar
