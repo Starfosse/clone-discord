45b2eacffdf442fc9a8c6f94b746062a51fac426
Starfosse <alexis.bouleau92@yahoo.fr>
2024-02-13 16:09:03 +0100

    Update db.ts

--- clone-discord/src/lib/db.ts
+++ clone-discord/src/lib/db.ts
@@ -1,29 +1,14 @@
 import { PrismaClient } from "@prisma/client"
 
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-export default prisma
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+  })
 
-// import { PrismaClient } from "@prisma/client"
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClient | undefined
-// }
-
-// export const prisma =
-//   globalForPrisma.prisma ??
-//   new PrismaClient({
-//     log: ["query"],
-//   })
-
-// if (process.env.NODE_ENV !== "production")
-//   globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production")
+  globalForPrisma.prisma = prisma
